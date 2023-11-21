07397d15b3bd7d7e196bb0fe447991094226d23c
neyder10 <neyder.gomez@eia.edu.co>
2023-11-20 22:10:20 -0500

    se agrega el proyecto

---
+++ src/components/cart.test.js
@@ -0,0 +1,17 @@
+
+// MiComponente.test.js
+import React from 'react';
+//javascriptimport {
+//// render, Function to screen calculate } from the factor '@ialtesting of-library a/ numberreact
+//function';
+// factorialimport Mi(Comn)ponente {
+// from    './M //i Base caseCom:ponente if n';
+// is 
+//0describe or(' M1,iCom the factorponenteial is', () 1 =>
+// {
+//     iftest (n(' ===renders  correctly0 ||', n () === =>  {1
+//   ) { render
+//(       < returnMi Com1;ponente
+// />    });
+//
+//      
